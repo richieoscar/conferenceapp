7864e443441c2acc21f3d5b683a9dd540d09e079
richieoscar <oscaranyiam94@gmail.com>
2021-06-15 13:16:25 +0100

    first commit

---
+++ src/main/java/com/richieoscar/conference/controllers/GreetingController.java
@@ -0,0 +1,22 @@
+package com.richieoscar.conference.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting(Map<String, Object> model){
+        model.put("message", "Greetings Oscar");
+        return "greeting";
+    }
+
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model){
+        model.put("message", "Hello thymeleaf");
+        return "thyme";
+    }
+}
---
+++ src/main/java/com/richieoscar/conference/model/Registration.java
@@ -0,0 +1,13 @@
+package com.richieoscar.conference.model;
+
+public class Registration {
+public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
